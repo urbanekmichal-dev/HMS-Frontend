a48cd7ba412c75049e1b546bc96e1842e09bccf8
Michal <urbanekmichal.98@gmail.com>
2022-01-08 22:50:27 +0100

    zmiany w restrukturyzacji

---
+++ src/app/reservation/shared/booking.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { BookingService } from 'src/shared/booking.service';
+
+
+
+describe('BookingService', () => {
+  let service: BookingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BookingService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/reservation/shared/image.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { ImageService } from 'src/shared/image.service';
+
+
+
+describe('ImageService', () => {
+  let service: ImageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ImageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
